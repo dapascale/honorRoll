d2d641261fcd1dd5725a5a956db56f79cbf57cde
Daniel <dapascale@gmail.com>
2021-01-11 15:41:00 -0500

    all styles and layout complete - still basic JS to add

---
+++ script.js
@@ -0,0 +1,15 @@
+// Pull in all of the DOM elements we will need: Form, and each input
+
+// DOM ELEMENTS
+
+const form = document.getElementById('form');
+
+// EVENT LISTENERS
+
+// Add an event listener for the form submit
+form.addEventListener('submit', function(e) {
+    // Prevent the form submit from refreshing the page
+    e.preventDefault();
+    // Direct user to subscription/plans page 
+})
+
